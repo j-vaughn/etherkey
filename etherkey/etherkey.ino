cda1d13dba627b58f2998f30d4523e9d68d82f68
j-vaughn <107257982+j-vaughn@users.noreply.github.com>
2024-02-25 17:00:24 -0600

    Update etherkey.ino
    Updated to work with Leonardo boards with code from https://www.sjoerdlangkemper.nl/2022/11/16/running-etherkey-on-arduino-leonardo/

--- etherkey/etherkey.ino
+++ etherkey/etherkey.ino
@@ -1,5 +1,28 @@
 #include "usb-keyboard.h"
 
+// Updated for Leonardo board with code from https://www.sjoerdlangkemper.nl/2022/11/16/running-etherkey-on-arduino-leonardo/
+#ifdef ARDUINO_AVR_LEONARDO
+#include "Keyboard.h"
+
+#define KEY_UP KEY_UP_ARROW
+#define KEY_DOWN KEY_DOWN_ARROW
+#define KEY_RIGHT KEY_RIGHT_ARROW
+#define KEY_LEFT KEY_LEFT_ARROW
+
+#define KEYPAD_PLUS KEY_KP_PLUS
+#define KEYPAD_0 KEY_KP_0
+
+#define KEY_ENTER KEY_RETURN
+#define KEY_SPACE ' '
+
+#define MODIFIERKEY_CTRL KEY_LEFT_CTRL
+#define MODIFIERKEY_ALT KEY_LEFT_ALT
+#define MODIFIERKEY_SHIFT KEY_LEFT_SHIFT
+#define MODIFIERKEY_GUI KEY_LEFT_GUI
+
+#define keyboard_leds 0
+#endif
+
 char in_ascii;
 char kbd_buff[KBD_BUFFSZ];
 int kbd_idx = 0;
